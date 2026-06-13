21d5e256325b6b1376f5c6472d89579173154295
Sahibjeet_Singh <sahibjeetsingh321@gmail.com>
2026-06-13 16:49:38 +0530

    Create My_program_with_for-loop.py

---
+++ My_program_with_for-loop.py
@@ -0,0 +1,26 @@
+# name = "Sahib"
+# for i in name:
+#     print(i)
+# for i in name:
+#     print(i, end=".")
+#     if(i=="b"):#This if is part of for loop te jado interpreter b te pahunchoo ta oh eh special message deyu.
+#         print("\n b for Baba banda singh Bahadur Ji.")
+# colors =["Red","Green","Blue","Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i,end=":")# Eh further harek color de characters nu print karu.
+# for number in range(100):
+#     print(number+1)# 1 to lai ke 100 tak numbers print honge.
+# for k in range(1,2001):
+#     print("Waheguru")#2000 vari Waheguru print hou
+# for k in range(1,2001):
+#     print(k)# 1 to 2000 takk ginti print hou
+for i in range(100,-1,-1):#100 to lai ke 1 takk reverse takk print hou.  
+    print(i)
+
+
+
+
+
+
