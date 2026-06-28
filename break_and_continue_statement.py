d55157220f40e46823b8d3f7895b536a4891de89
Sahibjeet_Singh <sahibjeetsingh321@gmail.com>
2026-06-28 12:29:26 +0530

    complete break and continue

---
+++ break_and_continue_statement.py
@@ -0,0 +1,24 @@
+# for i in range(12):#Apa 11 tak ginti print kara rahe ha.Loop i=11 takk challu.
+#     if(i==10):
+#         print("Skip the iteration")#5*10 layi print nahi hou.
+#         continue
+#     print("5 into",i,"is :",5*i)
+# for i in range(12):
+#     if(i==11):
+#         print("Loop chadd ke chale jao.")
+#         break
+#Python de andar do-while loop use karan da tarika
+# Step 1:variable initialization
+# while True:#T capital chahidi hai
+#     print(statement)
+#     increment or decrement
+#     if(oh condition jis de true hon te hi loop challni chahidi hai ):
+#         break 
+'''do while ikk exit contol loop hai te while entry control loop hai bhav ki jekar do-while de andar di condition false vi hove taa vi loop ik vaar challu.'''
+i=0
+while True:
+    print("Singh is King")
+    i=i+1
+    if(i>=0):
+        break
+
